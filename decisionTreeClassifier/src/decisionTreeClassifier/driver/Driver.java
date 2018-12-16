4be768d64f6b1f9ee9e91d0645f1ed5d8e622e86
Gregory Wint <gregwint95@gmail.com>
2018-12-16 15:05:29 -0500

    Added directory structure copied from Design Patterns course assignments

---
+++ decisionTreeClassifier/src/decisionTreeClassifier/driver/Driver.java
@@ -0,0 +1,98 @@
+package airportSecurityState.driver;
+import util.ProcessorI;
+import util.Results;
+import util.StdoutDisplayInterface;
+import java.io.File;
+import java.io.IOException;
+import util.FileProcessor;
+import util.MyLogger;
+import airportSecurityState.AirportContextI;
+import airportSecurityState.AirportSecurity;
+
+/**
+ * @author Gregory Wint
+ *
+ */
+
+/**
+ * Class used to house the main method responsible for creating the objects
+ * needed to calculate the sum of a collection of prime numbers.
+ */
+public class Driver {
+  /**
+   * Grabs names of the file containing a list of integers, the
+   * number of threads to use while calculating the sum of the primes in
+   * that list, and the debug value to use during execution.
+   * @return No return value.
+   */
+  public static void main(String[] args) {
+
+    /*
+     * As the build.xml specifies the arguments as argX, in case the
+     * argument value is not given java takes the default value specified in
+     * build.xml. To avoid that, below condition is used
+     */
+    if(args.length != 3 ||
+       args[0].equals("${arg0}") ||
+       args[1].equals("${arg1}") ||
+       args[2].equals("${arg2}")) {
+      System.err.println(
+       "Error: Incorrect number of arguments. Program accepts 3 arguments.");
+      System.exit(0);
+    }
+
+    // Read in input filename
+    CharSequence inputFilename = args[0];
+    try {
+      File testFile = new File(inputFilename.toString());
+      if(!testFile.exists()) {
+        System.err.printf("%s does not exist\n", inputFilename.toString());
+        System.exit(1);
+      }
+    }
+    catch(NullPointerException e) {
+      System.err.println("Pathname to input file cannot be null");
+      System.err.println(e);
+      System.exit(1);
+    }
+    finally {}
+
+    // Read in output filename
+    CharSequence outputFilename = args[1];
+    try {
+      File testFile = new File(outputFilename.toString());
+      if(testFile.exists() && testFile.length() > 0) {
+        throw new IllegalArgumentException("Cannot write to non-empty output file.  Please pick another/delete file/empty out the file");
+      }
+    }
+    catch(NullPointerException e) {
+      System.err.println("Pathname to output file cannot be null");
+      System.err.println(e);
+      System.exit(1);
+    }
+    finally {}
+
+    // Read in debug value
+    CharSequence debugValueStr = args[2];
+    int debugValue = -1;
+
+    try {
+      debugValue = Integer.parseInt(debugValueStr.toString());
+      if(debugValue < 0 || debugValue > 4) {
+        throw new IllegalArgumentException("The debug value must be an integer falling between 0 and 4, inclusive.");
+      }
+    }
+    catch(NumberFormatException e) {
+      System.err.println("The debug value must be an integer");
+      System.err.println(e);
+      System.exit(1);
+    }
+    finally {}
+
+    MyLogger.setDebugValue(debugValue);
+
+    AirportContextI airportSecurity = new AirportSecurity(inputFilename,
+                                                          outputFilename);
+    airportSecurity.processProhibitedItems();
+  }
+}
