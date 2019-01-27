88ee4828e1f04cc927b6b0287e90602692d0c3ac
Gregory Wint <gregwint95@gmail.com>
2019-01-27 16:39:46 -0500

    Removed code for assigning a label to a node frome Id3Algorithm since it already appears in the LabelAssigner visitor

--- decisionTreeClassifier/src/decisionTreeClassifier/classifier/ID3Algorithm.java
+++ decisionTreeClassifier/src/decisionTreeClassifier/classifier/ID3Algorithm.java
@@ -134,48 +134,6 @@ public class ID3Algorithm implements TrainingStrategy {
     System.out.println(splitFeatureIdx);
   }
 
-  private double getLabel(Node aNode) {
-    List<Integer> sampleIndices = aNode.getSampleIndices();
-    double classLabel = 0.0;
-    if(sampleIndices.size() == 0) {
-      classLabel = this.getLabel(aNode.getParent());
-    }
-    else {
-      int numSamples = sampleIndices.size();
-      Map<Double, Integer> classCounts = new HashMap<>();
-      for(Integer sampleIdx : sampleIndices) {
-        if(sampleIdx == null) {
-          throw new IllegalArgumentException("List of sample indices should not be null");
-        }
-        int i = sampleIdx.intValue();
-        Double label = classes.get(sampleIdx, 0);
-        if(label == null) {
-          throw new IllegalArgumentException("Every class label should be non-null");
-        }
-
-        if(!classCounts.containsKey(label)) {
-          classCounts.put(label, 1);
-        }
-        else {
-          classCounts.put(label, classCounts.get(label).intValue() + 1);
-        }
-
-        Set<Double> allLabels = classCounts.keySet();
-        Iterator<Double> labelIter = allLabels.iterator();
-        Iterator<Double> initializer = allLabels.iterator();
-        classLabel = initializer.next();
-
-        while(labelIter.hasNext()) {
-          Double aLabel = labelIter.next();
-          if(classCounts.get(aLabel) > classCounts.get(classLabel)) {
-            classLabel = aLabel.doubleValue();
-          }
-        }
-      }
-    }
-    return 0.0;
-  }
-
   /**
    */
   private List<Node> createChildren(int lowestEntropyFeatureIdx,
