6fcd087ff10b4db52ffa0434cd9c310d41e788e1
Gregory Wint <gregwint95@gmail.com>
2019-02-23 09:24:18 -0500

    Added a class to handle requests sent to the server for classification requests but need to specify the host and port number on which the classifier is running via the command line

---
+++ decisionTreeClassifier/src/decisionTreeClassifier/server/RequestHandler.java
@@ -0,0 +1,6 @@
+public class RequestHandler implements Runnable {
+  public void run() {
+    System.out.println("Now handling classifier request...");
+  }
+}
+
--- decisionTreeClassifier/src/decisionTreeClassifier/server/Server.java
+++ decisionTreeClassifier/src/decisionTreeClassifier/server/Server.java
@@ -5,15 +5,34 @@ import java.net.ServerSocket;
 import java.io.IOException;
 
 public class Server {
-  public static void start() throws IOException {
-    ServerSocket serverSocket = new ServerSocket(0);
+  public static void start() {
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(0);
+    }
+    catch(IOException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    }
+    finally {}
+
     System.out.println("Waiting for http requests..." +
                        serverSocket.getLocalPort());
     while(true) {
+      try {
+        serverSocket.accept();
+        // Launch thread to handle request
+      }
+      catch(IOException e) {
+        System.err.println(e.getMessage());
+        System.exit(1);
+      }
+      finally {}
     }
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
+    // parse command line arguments
     Server.start();
   }
 }
