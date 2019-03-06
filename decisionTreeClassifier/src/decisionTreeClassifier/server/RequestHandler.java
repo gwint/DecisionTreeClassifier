5a0bc2ca255d4cb9fbb9e484d6f0d222aa0fc08a
Gregory Wint <gregwint95@gmail.com>
2019-03-06 11:46:50 -0500

    Added methods to create an ndarray from a jsonarray and another to build a dataset object from ndarrays

--- decisionTreeClassifier/src/decisionTreeClassifier/server/RequestHandler.java
+++ decisionTreeClassifier/src/decisionTreeClassifier/server/RequestHandler.java
@@ -11,6 +11,10 @@ import org.json.JSONException;
 import org.json.JSONArray;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import util.NDArray;
+import util.Dataset;
 
 public class RequestHandler implements Runnable {
   private Socket clientConnection;
@@ -20,6 +24,79 @@ public class RequestHandler implements Runnable {
       throw new IllegalArgumentException("Client connection socket must not be null");
     }
     this.clientConnection = clientConnectionIn;
+  }
+
+  private NDArray<Double> createNDArray(JSONArray arr, int numRows,
+                                        int numCols) {
+    if(arr == null) {
+      throw new IllegalArgumentException("json array used to create ndarray must not be null");
+    }
+    if(numRows < 0 || numCols < 0) {
+      throw new IllegalArgumentException("Desired dimensions for ndarray must be positive");
+    }
+
+    NDArray<Double> ndArr = new NDArray<>(numRows, numCols);
+    int elementIndex = 0;
+
+    for(int currRow = 0; currRow < numRows; currRow++) {
+      for(int currCol = 0; currCol < numCols; currCol++) {
+        try {
+          ndArr.add(arr.getDouble(elementIndex), currRow, currCol);
+          elementIndex++;
+        }
+        catch(JSONException e) {
+          System.err.println("Mismatch between size of json array and dimensions of desired ndarray");
+          System.exit(1);
+        }
+        finally {}
+      }
+    }
+
+    return ndArr;
+  }
+
+  private Dataset getDataset(JSONArray features, JSONArray classes) {
+    if(features == null) {
+      throw new IllegalArgumentException("Features json array must not be null");
+    }
+    if(classes == null) {
+      throw new IllegalArgumentException("classes json array must not be null");
+    }
+
+    int numSamples = classes.length();
+
+    if(features.length() % numSamples != 0) {
+      throw new IllegalArgumentException("Number of features and number of class labels does not match");
+    }
+
+    int numFeaturesPerSample = features.length() / numSamples;
+
+    int desiredNumRows = numSamples;
+    int desiredNumCols = numFeaturesPerSample;
+
+    NDArray<Double> featuresNDArray = this.createNDArray(features,
+                                                         desiredNumRows,
+                                                         desiredNumCols);
+    NDArray<Double> classLabelsNDArray = this.createNDArray(classes,
+                                                            desiredNumRows,
+                                                            1);
+    return new Dataset(featuresNDArray, classLabelsNDArray);
+  }
+
+  private JSONArray getTestSampleClasses(JSONArray sampleIndices,
+                                         JSONArray all_data,
+                                         JSONArray all_classes) {
+    if(sampleIndices == null) {
+      throw new IllegalArgumentException("Array of sample indices must not be null");
+    }
+    if(all_data == null) {
+      throw new IllegalArgumentException("Array of feature data must not be null");
+    }
+    if(all_classes == null) {
+      throw new IllegalArgumentException("Array of class labels must not be null");
+    }
+
+    return new JSONArray();
   }
 
   public Object getJSONValue(JSONObject jsonObj, String key) {
@@ -45,8 +122,6 @@ public class RequestHandler implements Runnable {
   }
 
   public void run() {
-    System.out.println("Now handling classifier request...");
-
     InputStream datasetStream = null;
     try {
       datasetStream = this.clientConnection.getInputStream();
@@ -58,8 +133,6 @@ public class RequestHandler implements Runnable {
 
       boolean jsonEncountered = false;
       StringBuilder continueResponse = null;
-
-      System.out.println();
 
       Map<String, String> requestHeaderValues = new HashMap<>();
 
@@ -85,8 +158,6 @@ public class RequestHandler implements Runnable {
       }
       finally {}
 
-      System.out.println(bodySizeInBytes);
-
       continueResponse = new StringBuilder();
       continueResponse.append("HTTP/1.1 100 Continue\r\n\r\n");
       OutputStream socketWriteStream =
@@ -104,10 +175,6 @@ public class RequestHandler implements Runnable {
           nextByte = datasetStream.read();
         }
       }
-
-      System.out.println(datasetString);
-
-      System.out.println("Done reading");
 
       JSONObject jsonObj = new JSONObject(datasetString);
 
