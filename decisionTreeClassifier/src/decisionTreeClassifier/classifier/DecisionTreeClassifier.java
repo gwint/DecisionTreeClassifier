5ccce6e96523441b93e84f96cebf54239d186011
Gregory Wint <gregwint95@gmail.com>
2019-05-14 21:32:12 -0400

    Added some javadoc documentation to DecisionTreeClassifier.java

--- decisionTreeClassifier/src/decisionTreeClassifier/classifier/DecisionTreeClassifier.java
+++ decisionTreeClassifier/src/decisionTreeClassifier/classifier/DecisionTreeClassifier.java
@@ -30,17 +30,33 @@ public class DecisionTreeClassifier {
     this.maxHeight = maxHeightIn;
   }
 
+  /**
+   * Returns the maximum height that the decision tree can grow to during
+   * training.
+   *
+   * @return the maximum height that the decision tree can attain
+   */
   public int getDesiredMaxHeight() {
     return this.maxHeight;
   }
 
-  public void setDataset(Dataset datasetIn) {
+  private void setDataset(Dataset datasetIn) {
     if(datasetIn == null) {
       throw new IllegalArgumentException("Training dataset containing training features must not be null");
     }
     this.dataset = datasetIn;
   }
 
+  /**
+   * Builds decision tree based on dataset and list of indices marking each
+   * sample as either a training sample or a testing sample.
+   *
+   * @param dataset The dataset to use to construct the decision tree
+   * @param trainingSamples The list of indices of samples in the dataset to
+   *                        be used for training
+   * @param testingSamples The list of indices of samples in the dataset to be
+   *                       be used for testing
+   */
   public void train(Dataset dataset,
                     List<Integer> trainingSamples,
                     List<Integer> testingSamples) {
@@ -61,6 +77,14 @@ public class DecisionTreeClassifier {
                                                  this.getDesiredMaxHeight());
   }
 
+  /**
+   * Builds decision tree based on dataset where samples are randomly assigned
+   * to training and testing sets based on the proportion that should appear
+   * in the training set.
+   *
+   * @param dataset The dataset to use to construct the decision tree
+   * @param proportion Proportion of samples from dataset to use for training
+   */
   public void train(Dataset dataset, double proportion) {
     if(proportion < 0) {
       throw new IllegalArgumentException("Training proportion must be non-negative");
@@ -75,10 +99,21 @@ public class DecisionTreeClassifier {
                split.get(DecisionTreeClassifier.TESTING_SAMPLES));
   }
 
+  /**
+   * Returns the dataset used to train the classifier
+   *
+   * @return The dataset used to train the classifier
+   */
   public Dataset getDataset() {
     return this.dataset;
   }
 
+  /**
+   * Return the list of indices of samples from the dataset used for
+   * testing.
+   *
+   * @return The indices from the dataset of the samples used for testing
+   */
   public List<Integer> getTestingSamples() {
     return this.testingSamples;
   }
@@ -90,6 +125,7 @@ public class DecisionTreeClassifier {
   public NDArray<Double> predict(NDArray<Double> testSample) {
     NDArray<Double> prediction = new NDArray<>(1, 1);
     double predictedClass = this.getLabel(this.trainedClassifier, testSample);
+    prediction.add(predictedClass, 0, 0);
     return prediction;
   }
 
@@ -109,7 +145,34 @@ public class DecisionTreeClassifier {
   }
 
   private double getLabel(Node root, NDArray<Double> testSample) {
-    return 0.0;
+    List<Node> children = root.getChildren();
+    if(children == null) {
+      return root.getClassNum();
+    }
+
+    List<Interval> intervals = root.getSplitAttributeIntervals();
+    Integer splitAttributeIdx = root.getSplitAttributeIndex();
+    if(splitAttributeIdx == null) {
+      throw new IllegalStateException("Non-leaf node must have a non-null split attribute");
+    }
+
+    Double sampleSplitAttributeValue =
+                           testSample.get(0, splitAttributeIdx.intValue());
+
+    int i = 0;
+    while(i < intervals.size()) {
+      if(sampleSplitAttributeValue >= intervals.get(i).getStart() &&
+         sampleSplitAttributeValue <= intervals.get(i).getEnd()) {
+        break;
+      }
+      i++;
+    }
+
+    if(i == intervals.size()) {
+      throw new IllegalStateException("Sample cannot be placed into interval");
+    }
+
+    return this.getLabel(children.get(i), testSample);
   }
 
   private double getLabel(Node root, int sampleIdx) {
