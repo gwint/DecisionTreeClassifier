cc2b73048cc39430be5b61094fb90ab6f0830d1a
Gregory Wint <gregwint95@gmail.com>
2020-02-21 19:43:06 -0500

    Add methods to run wget command to grab data from a given url

--- helper.cpp
+++ helper.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <sstream>
+#include <cstdlib>
+#include <iostream>
 
 #include "helper.hpp"
 #include "mytypes.hpp"
@@ -38,3 +40,30 @@ my::multiple_sample_classes readClasses(std::string classesFileName) {
     return classes;
 }
 
+std::string get(const std::string& url) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(url.c_str(), "r");
+    if(pipe == NULL) {
+        std::cerr << "popen() call failed!\n";
+        exit(1);
+    }
+
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+
+    return "";
+}
+
+my::training_data getData(const std::string& url) {
+    my::training_data data;
+
+    return data;
+}
--- helper.hpp
+++ helper.hpp
@@ -7,5 +7,7 @@
 
 my::multiple_sample_features readFeatures(std::string);
 my::multiple_sample_classes readClasses(std::string);
+std::string get(const std::string&);
+my::training_data getData(const std::string&);
 
 #endif
